09fc6123f57d24bfafe4fa4120c62f24a910cb64
reis-iken <144738295+reis-iken@users.noreply.github.com>
2024-02-12 23:57:11 -0700

    code.js

--- code.js
+++ code.js
@@ -1,8 +1,8 @@
 function insertionSortReverse(array) {
-    for (let i = array.length - 1; i > 0; i--) {
+    for (let i = array.length - 1; i >= 0; i--) {
         let value = array[i];
         let j;
-        for (j = i; j > 0 && array[j-1] < value; j--) {
+        for (j = i; j >= 0 && array[j-1] < value; j--) {
             array[j] = array[j-1]; }
         array[j] = value; }
     return array;
